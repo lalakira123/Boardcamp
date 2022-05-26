056e40c5cef21cd4fb885b4ea2d35685623f9477
Laerte <laerteakira@gmail.com>
2022-05-26 13:27:10 -0300

    fix: post /games Columns Sensitive

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -17,7 +17,7 @@ export async function listGames(req, res) {
 export async function insertGame(req, res) {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        if( name.trim() === "" || stockTotal < 1 || pricePerDay < 1) return res.sendStatus(400);
+        if( name.trim() === "" || stockTotal <= 0 || pricePerDay <= 0) return res.sendStatus(400);
 
         const existId = await connection.query('SELECT * FROM categories WHERE id = $1;', [categoryId]);
         if( !existId.rows[0]?.id ) return res.sendStatus(400);
@@ -25,10 +25,11 @@ export async function insertGame(req, res) {
         const existName = await connection.query('SELECT * FROM games WHERE name = $1;', [name]);
         if( existName.rows[0]?.name ) return res.sendStatus(409);
 
-        await connection.query(`INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay)
-            VALUES($1, $2, $3, $4, $5);
+        await connection.query(`
+            INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+            VALUES ($1, $2, $3, $4, $5);
         `, [name, image, stockTotal, categoryId, pricePerDay]);
-
+        
         res.sendStatus(201);
     } catch (error) {
         res.status(404).send('Não foi possível conectar ao Banco');
