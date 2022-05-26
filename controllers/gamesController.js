700418ab82c7496401dd2745ffc15ef435d3ff78
Laerte <laerteakira@gmail.com>
2022-05-26 11:38:39 -0300

    fix: get /games && feat: post /games

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,9 +1,11 @@
 import connection from './../database.js';
 
 export async function listGames(req, res) {
-    const { name } = req.query;
+    let filter = req.query.name;
     try {
-        const query = await connection.query(`SELECT * FROM games WHERE LOWER(name) LIKE LOWER('$1%');`, [name]);
+        if( !filter ) filter = "";
+
+        const query = await connection.query(`SELECT * FROM games WHERE LOWER(name) LIKE LOWER('${filter}%');`);
         const games = query.rows;
 
         res.status(200).send(games);  
@@ -13,9 +15,22 @@ export async function listGames(req, res) {
 }
 
 export async function insertGame(req, res) {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        
+        if( name.trim() === "" || stockTotal < 1 || pricePerDay < 1) return res.sendStatus(400);
+
+        const existId = await connection.query('SELECT * FROM categories WHERE id = $1;', [categoryId]);
+        if( !existId.rows[0]?.id ) return res.sendStatus(400);
+
+        const existName = await connection.query('SELECT * FROM games WHERE name = $1;', [name]);
+        if( existName.rows[0]?.name ) return res.sendStatus(409);
+
+        await connection.query(`INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay)
+            VALUES($1, $2, $3, $4, $5);
+        `, [name, image, stockTotal, categoryId, pricePerDay]);
+
+        res.sendStatus(201);
     } catch (error) {
-        
+        res.status(404).send('Não foi possível conectar ao Banco');
     }
 }
